e76c4e92d741153c5194efed312a36a7f002ad4e
RAHussaini <ali.hussaini2212@gmail.com>
2020-09-29 12:54:02 +0200

    renamed main file

---
+++ CSharpFirstProject/CSharpFirstProject/HelloCSharp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpFirstProject
+{
+    class HelloCSharp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(" this is just the begining of the end  !!!");
+        }
+    }
+}
